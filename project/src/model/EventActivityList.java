f7a09375c1a39b1dea8f223269a368e5d721994a
jullganner <j3blol@gmail.com>
2014-03-07 14:01:40 +0100

    fuck yes det funkar

--- project/src/model/EventActivityList.java
+++ project/src/model/EventActivityList.java
@@ -36,20 +36,8 @@ public class EventActivityList extends ArrayAdapter<String>{
 		LayoutInflater inflater = context.getLayoutInflater();
 		listItemView = inflater.inflate(R.layout.list_item, null, true);
 		
-		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		
-		eventTitle = (TextView) listItemView.findViewById(R.id.list_item_title);
-		eventDuration = (TextView) listItemView.findViewById(R.id.list_item_duration);
-		eventImage = (ImageView) listItemView.findViewById(R.id.list_item_image);
-		
-		System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-		
-		EventActivity selectedEvent = parkedEvents[position];
-		eventTitle.setText("" + selectedEvent.getName());
-		eventDuration.setText("" + selectedEvent.getLength());
-		eventImage.setImageResource(selectedEvent.getImage());	
-		
-		System.out.println("ccccccccccccccccccccccccccccccccccccccccccccccccc");
+		buildComponents();
+		setResourcesForComponents();	
 		
 		return listItemView;
 	}
@@ -58,14 +46,19 @@ public class EventActivityList extends ArrayAdapter<String>{
 	 * Build the three components for the list items
 	 */
 	private void buildComponents() {
-		
+		eventTitle = (TextView) listItemView.findViewById(R.id.list_item_title);
+		eventDuration = (TextView) listItemView.findViewById(R.id.list_item_duration);
+		eventImage = (ImageView) listItemView.findViewById(R.id.list_item_image);
 	}
 	
 	/**
 	 * Sets the resources for all of the components created in the list item
 	 */
 	private void setResourcesForComponents() {
-		
+		EventActivity selectedEvent = parkedEvents[position];
+		eventTitle.setText("" + selectedEvent.getName());
+		eventDuration.setText("" + selectedEvent.getLength());
+		eventImage.setImageResource(selectedEvent.getImage());	
 	}
 	
 
