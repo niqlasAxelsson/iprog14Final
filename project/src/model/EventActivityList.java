98c84eabcc91bdd5fec04a329d819b780606ca1c
jullganner <j3blol@gmail.com>
2014-03-07 13:15:27 +0100

    Test if github works

--- project/src/model/EventActivityList.java
+++ project/src/model/EventActivityList.java
@@ -1,13 +1,65 @@
 package model;
 
-import android.content.Context;
+import com.example.pl4nn3r3000.R;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class EventActivityList extends ArrayAdapter<String>{
+public class EventActivityList extends ArrayAdapter<EventActivity>{
+	
+	private Activity context;
+	private AgendaModel model;
+	private int position;
+	private TextView eventTitle;
+	private TextView eventDuration;
+	private ImageView eventImage;
+	private View listItemView;
+	private EventActivity[] freeEvents;
+	
 
-	public EventActivityList(Context context, int resource) {
-		super(context, resource);
-		// TODO Auto-generated constructor stub
+	public EventActivityList(Activity context, AgendaModel model, EventActivity[] freeEvents) {
+		super(context, R.layout.list_item);
+		this.context = context;
+		this.model = model;
+		this.freeEvents = freeEvents;
+		
 	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		this.position = position;
+		LayoutInflater inflater = context.getLayoutInflater();
+		listItemView = inflater.inflate(R.layout.list_item, null, true);
+		
+		buildComponents();
+		setResourcesForComponents();
+		
+		return listItemView;
+	}
+
+	/**
+	 * Build the three components for the list items
+	 */
+	private void buildComponents() {
+		eventTitle = (TextView) listItemView.findViewById(R.id.list_item_title);
+		eventDuration = (TextView) listItemView.findViewById(R.id.list_item_duration);
+		eventImage = (ImageView) listItemView.findViewById(R.id.list_item_image);
+	}
+	
+	/**
+	 * Sets the resources for all of the components created in the list item
+	 */
+	private void setResourcesForComponents() {
+		EventActivity selectedEvent = freeEvents[position];
+		eventTitle.setText(selectedEvent.getName());
+		eventDuration.setText(selectedEvent.getLength());
+		eventImage.setImageResource(selectedEvent.getImage());	
+	}
+	
 
 }
