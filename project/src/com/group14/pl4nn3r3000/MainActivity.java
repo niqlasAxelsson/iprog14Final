840471b2a1fbeaf618af06371701c6f476569df9
jullganner <j3blol@gmail.com>
2014-03-07 13:12:17 +0100

    lalal
    dadad

--- project/src/com/group14/pl4nn3r3000/MainActivity.java
+++ project/src/com/group14/pl4nn3r3000/MainActivity.java
@@ -1,5 +1,12 @@
 package com.group14.pl4nn3r3000;
 
+import java.util.List;
+
+import model.AgendaApplication;
+import model.AgendaModel;
+import model.EventActivity;
+import model.EventActivityList;
+
 import com.example.pl4nn3r3000.R;
 
 import android.app.Activity;
@@ -9,21 +16,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private AgendaModel model;
+	private EventActivityList adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//create the horizontal listview
-		HorizontalListView listview = (HorizontalListView) findViewById(R.id.listview);
-        listview.setAdapter(mAdapter);
+		//get the application model
+		model = ((AgendaApplication) this.getApplication()).getModel();
+		model.addExampleData();
+		EventActivity[] parkedActivities= model.getParkedActivitiesArray();
 		
-		//starts fragment
+		// create the horizontal listview
+		HorizontalListView listview = (HorizontalListView) findViewById(R.id.listview);
+		adapter = new EventActivityList(this, model, parkedActivities);
+		listview.setAdapter(adapter);
+
+		// starts fragment
 		AllDaysFragment frag = new AllDaysFragment();
 		FragmentManager manager = getFragmentManager();
 		FragmentTransaction transaction = manager.beginTransaction();
@@ -32,40 +49,41 @@ public class MainActivity extends Activity {
 	}
 
 	
-	 private static String[] dataObjects = new String[]{ "Text #1",
-	        "Text #2",
-	        "Text #3", 
-	        "fnhiosw,","fdsfvs" , "few", "vfdsv","Maddafukka", "qwerty"};
-	    
-	    private BaseAdapter mAdapter = new BaseAdapter() {
-	 
-	        @Override
-	        public int getCount() {
-	            return dataObjects.length;
-	        }
-	 
-	        @Override
-	        public Object getItem(int position) {
-	            return null;
-	        }
-	 
-	        @Override
-	        public long getItemId(int position) {
-	            return 0;
-	        }
-	 
-	        @Override
-	        public View getView(int position, View convertView, ViewGroup parent) {
-	            View retval = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, null);
-	            TextView title = (TextView) retval.findViewById(R.id.list_item_title);
-	            title.setText(dataObjects[position]);
-	            
-	            return retval;
-	        }
-
-
-	        
-	    };
 	
+	//allt här under ska tas bort när EventActivityList är klar
+	
+	private static String[] dataObjects = new String[] { "Text #1", "Text #2",
+			"Text #3", "fnhiosw,", "fdsfvs", "few", "vfdsv", "Maddafukka",
+			"qwerty" };
+
+	private BaseAdapter mAdapter = new BaseAdapter() {
+
+		@Override
+		public int getCount() {
+			return dataObjects.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View retval = LayoutInflater.from(parent.getContext()).inflate(
+					R.layout.list_item, null);
+			TextView title = (TextView) retval
+					.findViewById(R.id.list_item_title);
+			title.setText(dataObjects[position]);
+
+			return retval;
+		}
+
+	};
 
 }
