e3a599f73c3abf09e2eff546be2f54773cb6cf9f
jullganner <j3blol@gmail.com>
2014-03-14 14:16:43 +0100

    Viewclass for CreateEventActivity
    adad

--- project/src/com/group14/pl4nn3r3000/CreateEventActivity.java
+++ project/src/com/group14/pl4nn3r3000/CreateEventActivity.java
@@ -1,19 +1,19 @@
 package com.group14.pl4nn3r3000;
 
-import view.ActionBarView;
 import model.AgendaApplication;
 import model.AgendaModel;
 import model.EventActivity;
+import view.ActionBarView;
+import view.CreateEventView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.pl4nn3r3000.R;
@@ -31,186 +31,155 @@ public class CreateEventActivity extends Activity {
 	String descriptionText;
 	int activityType = 8;
 	
+	AgendaModel model;
+	CreateEventView View;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_event);
-		setComponents();
-		setRbClickListeners();
-		buildActionBar();
+		model = ((AgendaApplication) getApplication()).getModel();
+		View = new CreateEventView(this.findViewById(android.R.id.content),model,this);
 		
-		doneButton.setOnClickListener(new View.OnClickListener(){
+		setClickOnDoneButton();
+		setRbClickListeners();
+		
+	}
+	
+	
+	private void setClickOnDoneButton(){
+		
+		View.getDoneButton().setOnClickListener(new View.OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				AgendaModel model = ((AgendaApplication) getApplication()).getModel();
-				getFields();
-				EventActivity newEventActivity = new EventActivity(activityName,descriptionText,duration,activityType);
+				//AgendaModel model = ((AgendaApplication) getApplication()).getModel();
+				View.getFields();
+				EventActivity newEventActivity = new EventActivity(View.getActivityName(),View.getDescriptionText(),View.getDuration(),View.getActivityType());
 				model.addParkedActivity(newEventActivity);
 				Intent i = new Intent(getBaseContext(),MainActivity.class);
 				startActivity(i);
 			}
 			
 		});
-		
-		
 	}
 	
 	
 
-	private void buildActionBar() {
-		ActionBarView view = new ActionBarView(this, ViewGroup.INVISIBLE);
-	}
-
-
-
-	/**
-	 * sets the compontents in the activity
-	 */
-	private void setComponents(){
-		
-		activityField = (TextView) findViewById(R.id.acitivty_name_id);
-		descriptionField = (TextView) findViewById(R.id.description_text_id);
-		doneButton = (Button)findViewById(R.id.done_button_id);
-		durationPicker = (NumberPicker) findViewById(R.id.durationPicker_id);
-		
-		durationPicker.setMaxValue(24);
-		durationPicker.setMinValue(1);
-		durationPicker.setWrapSelectorWheel(false); 
-		
-		//radiobuttons
-		rbWorkout = (RadioButton) findViewById(R.id.workout_rb);
-		rbMeal = (RadioButton) findViewById(R.id.meal_rb);
-		rbMeeting = (RadioButton) findViewById(R.id.meeting_rb);
-		rbParty = (RadioButton) findViewById(R.id.party_rb);
-		rbStudies = (RadioButton) findViewById(R.id.studies_rb);
-		rbWork = (RadioButton) findViewById(R.id.work_rb);
-		rbPleasure = (RadioButton) findViewById(R.id.pleasure_rb);
-		rbOther = (RadioButton) findViewById(R.id.other_rb);
-		
-	}
 	
-	/**
-	 * get all the fields from the eventeacitivty fields
-	 */
-	private void getFields(){
-		
-		activityName = activityField.getText().toString();
-		descriptionText = descriptionField.getText().toString();
-		//TODO
-	 /// change this
-		duration = durationPicker.getValue();
-		
-	}
-
+	
 
 	
 	private void setRbClickListeners() {
 		
-		rbWorkout.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[0].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(true);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 1;
+	           View.getAllRadioButtons()[0].setChecked(true);
+	           View.getAllRadioButtons()[1].setChecked(false);
+	           View.getAllRadioButtons()[2].setChecked(false);
+	           View.getAllRadioButtons()[3].setChecked(false);
+	           View.getAllRadioButtons()[4].setChecked(false);
+	           View.getAllRadioButtons()[5].setChecked(false);
+	           View.getAllRadioButtons()[6].setChecked(false);
+	           View.getAllRadioButtons()[7].setChecked(false);
+	           View.setActivityType(1);
 	        }
 	    });
-		rbMeal.setOnClickListener(new OnClickListener() {
+		
+		View.getAllRadioButtons()[1].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(true);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 3;
+	        	  View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(true);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(false);
+		           View.setActivityType(3);
+	          
 	        }
 	    });
-		rbMeeting.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[2].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(true);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 2;
+	        	 View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(true);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(false);
+		           View.setActivityType(2);
 	        }
 	    });
-		rbParty.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[3].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(true);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 4;
+	        	View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(true);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(false);
+		           View.setActivityType(4);
+	        
 	        }
 	    });
-		rbStudies.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[4].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(true);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 5;
+	        	View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(true);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(false);
+	           View.setActivityType(5);
 	        }
 	    });
-		rbWork.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[5].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(true);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(false);
-	           activityType = 6;
+	        	View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(true);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(false);
+	           View.setActivityType(6);
 	        }
 	    });
-		rbPleasure.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[6].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(true);
-	           rbOther.setChecked(false);
-	           activityType = 7;
+	        	View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(true);
+		           View.getAllRadioButtons()[7].setChecked(false);
+		           View.setActivityType(7);
 	        }
 	    });
-		rbOther.setOnClickListener(new OnClickListener() {
+		View.getAllRadioButtons()[7].setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	           rbWorkout.setChecked(false);
-	           rbMeal.setChecked(false);
-	           rbMeeting.setChecked(false);
-	           rbParty.setChecked(false);
-	           rbStudies.setChecked(false);
-	           rbWork.setChecked(false);
-	           rbPleasure.setChecked(false);
-	           rbOther.setChecked(true);
-	           activityType = 8;
+	        	View.getAllRadioButtons()[0].setChecked(false);
+		           View.getAllRadioButtons()[1].setChecked(false);
+		           View.getAllRadioButtons()[2].setChecked(false);
+		           View.getAllRadioButtons()[3].setChecked(false);
+		           View.getAllRadioButtons()[4].setChecked(false);
+		           View.getAllRadioButtons()[5].setChecked(false);
+		           View.getAllRadioButtons()[6].setChecked(false);
+		           View.getAllRadioButtons()[7].setChecked(true);
+		           View.setActivityType(8);
 	        }
 	    });
+	    
 	}
 
 }
--- project/src/view/CreateEventView.java
+++ project/src/view/CreateEventView.java
@@ -3,29 +3,141 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import com.example.pl4nn3r3000.R;
+
 import model.AgendaModel;
 import android.app.Activity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class CreateEventView implements Observer {
-	
+
 	View view;
 	AgendaModel model;
 	Activity activity;
 	
-	public CreateEventView(View view,AgendaModel model, Activity activity){
-		
+	ActionBarView actionBar;
+	TextView activityField;
+	TextView descriptionField;
+	Button doneButton;
+	NumberPicker durationPicker;
+	RadioButton rbWorkout, rbMeal, rbMeeting, rbParty, rbStudies, rbWork,
+			rbPleasure, rbOther;
+
+	int duration;
+	String activityName;
+	String descriptionText;
+	int activityType = 8;
+
+	public CreateEventView(View view, AgendaModel model, Activity activity) {
+
 		this.view = view;
-		this.model = model;
-		this.activity=activity;
+		//this.model = model;
+		this.activity = activity;
+		buildActionBar();
+		setComponents();
+	}
+	
+	/**
+	 * builds the actionBar
+	 */
+	private void buildActionBar() {
+		actionBar = new ActionBarView(activity, ViewGroup.INVISIBLE);
+	}
+
+	/**
+	 * sets the COmponents in the view.
+	 */
+	private void setComponents() {
+
+		activityField = (TextView) view.findViewById(R.id.acitivty_name_id);
+		descriptionField = (TextView) view
+				.findViewById(R.id.description_text_id);
+		doneButton = (Button) view.findViewById(R.id.done_button_id);
+		durationPicker = (NumberPicker) view
+				.findViewById(R.id.durationPicker_id);
+
+		durationPicker.setMaxValue(24);
+		durationPicker.setMinValue(1);
+		durationPicker.setWrapSelectorWheel(false);
+
+		// radiobuttons
+		rbWorkout = (RadioButton) view.findViewById(R.id.workout_rb);
+		rbMeal = (RadioButton) view.findViewById(R.id.meal_rb);
+		rbMeeting = (RadioButton) view.findViewById(R.id.meeting_rb);
+		rbParty = (RadioButton) view.findViewById(R.id.party_rb);
+		rbStudies = (RadioButton) view.findViewById(R.id.studies_rb);
+		rbWork = (RadioButton) view.findViewById(R.id.work_rb);
+		rbPleasure = (RadioButton) view.findViewById(R.id.pleasure_rb);
+		rbOther = (RadioButton) view.findViewById(R.id.other_rb);
+
+	}
+	
+	public RadioButton[] getAllRadioButtons(){
+		
+		RadioButton allButtons[] = new RadioButton[8];
+		allButtons[0] = rbWorkout;
+		allButtons[1] = rbMeal;
+		allButtons[2] = rbMeeting;
+		allButtons[3] = rbParty;
+		allButtons[4] = rbStudies;
+		allButtons[5] = rbWork;
+		allButtons[6] = rbPleasure;
+		allButtons[7] = rbOther;
+		return allButtons;
+	}
+	/**
+	 * returns the doneBUtton
+	 * @return
+	 */
+	public Button getDoneButton(){
+		
+		return doneButton;
+	}
+	
+	/**
+	 * returns all the values in the field
+	 */
+	public void getFields(){
+		
+		activityName = activityField.getText().toString();
+		descriptionText = descriptionField.getText().toString();
+		//TODO
+		duration = durationPicker.getValue();
 		
 	}
+	
+	public String getActivityName(){
+		return activityName;
+	}
+	public String getDescriptionText(){
+		
+		return descriptionText;
+	}
+	public int getDuration()
+	{
+		return duration;
+	}
+	public int getActivityType(){
+		return activityType;
+	}
+	
+	public void setActivityType(int i){
+		
+		activityType = i;
+	}
+	
+	
 	
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
