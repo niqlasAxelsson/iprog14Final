af3c64e01d8593c360b6f567727a392fc18c7e9c
jullganner <j3blol@gmail.com>
2014-03-18 12:04:04 +0100

    text sätts

--- project/src/com/group14/pl4nn3r3000/SelectedDayFragment.java
+++ project/src/com/group14/pl4nn3r3000/SelectedDayFragment.java
@@ -1,20 +1,15 @@
 package com.group14.pl4nn3r3000;
 
-import model.AgendaApplication;
-import model.AgendaModel;
-
 import com.example.pl4nn3r3000.R;
 
+import model.AgendaApplication;
+import model.AgendaModel;
+import view.SelectedDayFragmentView;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 /**
@@ -26,15 +21,28 @@ import android.widget.TextView;
 public class SelectedDayFragment extends Fragment {
 
 	AllDaysFragment frag;
+	SelectedDayFragmentView view;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		AgendaModel model = ((AgendaApplication) this.getActivity().getApplication()).getModel();
-		View view = inflater.inflate(R.layout.selected_day_fragment_layout,
-				container, false);
-
-		return view;
+		//View view = inflater.inflate(R.layout.selected_day_fragment_layout,
+			//	container, false);
+		
+		view = new SelectedDayFragmentView(this.getActivity(),inflater.inflate(R.layout.selected_day_fragment_layout,container, false));
+		initTextView();
+		
+		
+		return view.getView();
+	}
+	
+	
+	private void initTextView(){
+		
+		AgendaModel model = ((AgendaApplication) this.getActivity().getApplication()).getModel();
+		view.getDayTitleTextView().setText(model.getSelectedDay().getDateString());
+		
 	}
 
 }
--- project/src/view/SelectedDayFragmentView.java
+++ project/src/view/SelectedDayFragmentView.java
@@ -1,5 +1,69 @@
 package view;
 
+import com.example.pl4nn3r3000.R;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+/**
+ * The selecteddayFragment View. 
+ * @author julle
+ *
+ */
 public class SelectedDayFragmentView {
 
+	Activity activity;
+	View view;
+	
+	ListView listView;
+	TextView dayTitle;
+	
+	public SelectedDayFragmentView(Activity activity,View view){
+		this.view = view;
+		this.activity=activity;
+		buildComponents();
+		
+	}
+	
+	/**
+	 * set the components
+	 */
+	private void buildComponents(){
+		
+		ListView listView = (ListView)view.findViewById(R.id.selectedDayListView);
+		dayTitle = (TextView) view.findViewById(R.id.selectedDayTextView);
+		
+		
+	}
+	
+	/**
+	 * returns the dayTitle TextView
+	 * @return
+	 */
+	public TextView getDayTitleTextView(){
+		
+		return dayTitle;
+	}
+	
+	/**
+	 * returns the View
+	 * @return
+	 */
+	public View getView(){
+		
+		return view;
+	}
+	
+	/**
+	 * returns the listView
+	 * @return
+	 */
+	public ListView getListView(){
+		
+		return listView;
+	}
+	
 }
