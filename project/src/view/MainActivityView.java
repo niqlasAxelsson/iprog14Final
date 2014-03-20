7f1f1a0680d0480b899860031cbc5f030e902680
jullganner <j3blol@gmail.com>
2014-03-20 12:51:26 +0100

    push it baby

--- project/src/view/MainActivityView.java
+++ project/src/view/MainActivityView.java
@@ -135,7 +135,7 @@ public class MainActivityView implements Observer {
 		
 		System.out.println("Inne i update");
 		
-		System.out.println(strings[0] + " " + strings[1]);
+		//System.out.println(strings[0] + " " + strings[1]);
 		if (strings[0].equals("ParkedActivityRemoved")) {
 			getActivityNamesList().remove(Integer.parseInt(strings[1]));
 
