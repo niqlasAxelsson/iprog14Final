ef6dabca38739e6654326dac81d618f4ef54533f
jullganner <j3blol@gmail.com>
2014-03-20 14:21:14 +0100

    parked

--- project/src/view/MainActivityView.java
+++ project/src/view/MainActivityView.java
@@ -147,6 +147,7 @@ public class MainActivityView implements Observer {
 		
 		if(strings[0].equals("ActivityParked")){
 			System.out.println("activity parked!");
+			getAdapter().notifyDataSetChanged();
 			
 		}
 
